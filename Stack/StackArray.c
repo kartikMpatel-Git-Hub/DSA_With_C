ac7185033c0b7b0304263d6c1e46c994d860c225
kartikMpatel <kartikpatel7892@gmail.com>
2025-02-12 00:37:53 +0530

    Stack Something Still Pending

---
+++ Stack/StackArray.c
@@ -0,0 +1,68 @@
+#include "stdio.h"
+#include "string.h"
+
+int isEmpty(int top){
+    if(top == -1){
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(int top,int n){
+    if((top+1) == n){
+        return 1;
+    }
+    return 0;
+}
+
+void push(int *arr,int ele,int *top,int n){
+    if(isFull(*top,n)){
+        printf("\nStack Is Full !");
+        return;
+    }
+    arr[++*top] = ele;
+}
+
+void peek(int *arr,int top){
+    if(isEmpty(top)){
+        printf("\nStack Is Empty !");
+        return;
+    }
+    printf("\n%d",arr[top]);
+}
+void pop(int *arr,int *top){
+    if(isEmpty(*top)){
+        printf("\nStack Is Empty !");
+        return;
+    }
+    --*top;
+}
+int main(){
+    int n = 5;
+    int arr[n];
+    int top = -1;
+    push(arr,1,&top,n);
+    push(arr,2,&top,n);
+    push(arr,3,&top,n);
+    push(arr,4,&top,n);
+    push(arr,5,&top,n);
+    push(arr,6,&top,n);
+
+    peek(arr,top);
+    pop(arr,&top);
+
+    peek(arr,top);
+    pop(arr,&top);
+
+    peek(arr,top);
+    pop(arr,&top);
+
+    peek(arr,top);
+    pop(arr,&top);
+
+    peek(arr,top);
+    pop(arr,&top);
+
+    pop(arr,&top);
+    peek(arr,top);
+}
---
+++ Stack/StackWithArrayCharacter.c
@@ -0,0 +1,68 @@
+#include "stdio.h"
+#include "stdlib.h"
+typedef struct
+{
+    int size;
+    int top;
+      *data;
+}Stack;
+
+int isEmpty(Stack *s){
+    if(s->top == -1)
+        return 1;
+    return 0;
+}
+int isFull(Stack *s){
+    if(s->top+1 == s->size)
+        return 1;
+    return 0;
+}
+void push(Stack* s,int data){
+    if(isFull(s)){
+        printf("\nOver FLow !");
+        return;
+    }
+    s->data[s->top+1] = data;
+    s->top++;
+}
+int peak(Stack* s){
+    if(isEmpty(s)){
+        printf("\nStack Is Empty !");
+        return -1;
+    }
+    return s->data[s->top];
+}
+void pop(Stack *s){
+    if(isEmpty(s)){
+        printf("\nStack Is Empty !");
+        return;
+    }
+    s->top--;
+}
+Stack* createStack(){
+    Stack *s = (Stack *)malloc(sizeof(Stack));
+    s->top = -1;
+    s->size = 5;
+    s->data = (int *)malloc(s->size * sizeof(int));
+    return s;
+}
+int main(){
+    Stack *s1 = createStack();
+    Stack *s2 = createStack();
+
+    push(s1,10);
+    push(s1,20);
+    push(s1,30);
+    push(s1,40);
+    push(s2,1);
+    push(s2,2);
+    push(s2,3);
+    push(s2,4);
+
+    printf("\n%d",peak(s1));
+    printf("\n%d",peak(s2));
+    pop(s1);
+    pop(s2);
+    printf("\n%d",peak(s1));
+    printf("\n%d",peak(s2));
+}
